07366ee2e01eb12079337a445dcf543a35da9e8c
Gabo Esquivel <contact@gaboesquivel.com>
2021-09-22 22:14:39 -0600

    wip: refactoring indexer code structure

--- src/mappings/index.ts
+++ src/mappings/index.ts
@@ -4,14 +4,17 @@ import { db } from '../lib/prisma'
 import { ContractMappings } from './types'
 
 export const createMappingsSubject = () => {
-  const mappings$ = new Subject<ContractMappings>()
+  const mappings$ = new Subject<ContractMappings[]>()
 
   log.info('Subscribing to contract mappings, refreshing every 1s ...')
   setInterval(async () => {
     try {
       const data = await db.mappings.findMany()
 
-      const mappings = data.map((row) => row.mappings as ContractMappings)
+      // load json files with proper type
+      const mappings = data.map(
+        (row) => row.mappings as unknown as ContractMappings,
+      )
 
       mappings$.next(mappings)
     } catch (error) {
