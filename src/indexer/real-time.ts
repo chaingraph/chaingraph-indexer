f03db3c7681109a6be98b1dfc00a5da01dd6107f
Roberto Lucas <andre.rlucas@outlook.com>
2022-12-14 23:44:08 -0600

    fix: table_row invalid p_key filter

--- src/indexer/real-time.ts
+++ src/indexer/real-time.ts
@@ -41,7 +41,7 @@ export const startRealTimeStreaming = async (
           return row.present
         })
         .map((row) => getChainGraphTableRowData(row, mappings_reader))
-        .filter((row) => row.primary_key && Boolean(row.primary_key.match(/(undefined|\[object object\])/g)))
+        .filter((row) => row.primary_key && Boolean(row.primary_key.match(/(undefined|\[object Object\])/g)))
 
       if (table_rows_deltas.length > 0) upsertTableRows(table_rows_deltas)
 
